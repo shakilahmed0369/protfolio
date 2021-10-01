2964b5bf7d8784fe57cc7ebd01b42ae6d605263b
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-10-02 00:37:25 +0600

    new changes

--- src/pages/show.jsx
+++ src/pages/show.jsx
@@ -4,30 +4,32 @@ import { portfolios } from "../data"
 import "../components/lovetodo.css"
 function Show({location}) {
  const newData = portfolios.Web.filter((value, index, arr) => {
-    return arr.indexOf(value) == location.state.index;
+    return arr.indexOf(value) === location.state.index;
   })
-
-  console.log(newData);
 
 
   return (
     <div>
       <Container >
         <Title side="left" title="Project view" />
-        <div style={{ paddingLeft:"50px", paddingRight: "0" }} className="love-to-do">
+        {
+          newData ?
+          (<div style={{ paddingLeft:"50px", paddingRight: "0" }} className="love-to-do">
           <div className="stickpad">
             <h4>{newData[0].name}</h4>
             <p style={{ marginTop: "5px" }}>{newData[0].description}</p>
 
             {
               newData[0].allImage.map((image) => {
-               return <img style={{ width: "100%", marginTop: "20px", borderRadius: "5px" }} src="https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg" alt="dsa" />
+               return <img key={image} style={{ width: "100%", marginTop: "20px", borderRadius: "5px" }} src="https://cdn.pixabay.com/photo/2015/12/04/14/05/code-1076536_960_720.jpg" alt="dsa" />
               })
             }
 
 
           </div>
-        </div>
+        </div>) : ''
+        }
+        
       </Container>
     </div>
   )
