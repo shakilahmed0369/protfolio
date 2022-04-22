be7ff79d4f81ebcec70b1d1b725ddd2a37c74e9c
Shakil Ahmed <51516043+shakilahmed0369@users.noreply.github.com>
2022-04-22 11:16:33 +0600

    chore: some changes on skill data

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,27 +1,26 @@
 export const skills = {
   Web: [
     { name: "Laravel", percent: 90 },
-    { name: "Rest Api", percent: 90 },
     { name: "Vue", percent: 80 },
-    { name: "React", percent: 75 },
-    { name: "Bootstrap", percent: 90 },
-    { name: "Tailwind", percent: 80 },
-    { name: "Lumen", percent: 90 },
-    { name: "sqlite", percent: 90 },
-    { name: "MySQL", percent: 85 },
-  ],
-  Others: [
-    { name: "Inertia", percent: 85 },
-    { name: "Sass", percent: 90 },
-    { name: "Redux", percent: 80 },
-    { name: "SVG", percent: 80 },
-  ],
-  Programming: [
+    { name: "Rest Api", percent: 90 },
     { name: "Php", percent: 90 },
     { name: "JavaScript", percent: 80 },
-    { name: "Python", percent: 50 },
-    { name: "C++", percent: 40 },  
+    { name: "Bootstrap", percent: 90 },
+    { name: "Tailwind", percent: 80 },
+    { name: "MySQL", percent: 85 },
   ],
+  // Others: [
+  //   { name: "Inertia", percent: 85 },
+  //   { name: "Sass", percent: 90 },
+  //   { name: "Redux", percent: 80 },
+  //   { name: "SVG", percent: 80 },
+  // ],
+  // Programming: [
+  //   { name: "Php", percent: 90 },
+  //   { name: "JavaScript", percent: 80 },
+  //   { name: "Python", percent: 50 },
+  //   { name: "C++", percent: 40 },  
+  // ],
   Tools: [
     { name: "Git", percent: 90 },
     { name: "Insomnia / Post Man", percent: 80 },
