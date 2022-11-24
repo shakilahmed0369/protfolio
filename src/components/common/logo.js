b0dbb48620e68982d30e5ff898774d8f796760e6
shakilahmed0369 <sakilhossain01969@gmail.com>
2022-11-25 02:54:58 +0600

    new build

--- src/components/common/logo.js
+++ src/components/common/logo.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export const Logo = () => {
   return (
-    <img style={{ "width": "50px" }} src="https://user-images.githubusercontent.com/51516043/136998908-0c01a1b2-8f62-43e2-9c70-179fc4a58a2a.png"/>
+    <img style={{ "width": "160px" }} src="../../logo.png"/>
   )
 }
